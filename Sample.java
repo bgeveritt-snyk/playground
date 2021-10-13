4cce701151eb1ffaebdcc36509783c073dc8e831
Bailey Everitt <72466036+bgeveritt-snyk@users.noreply.github.com>
2021-10-13 11:44:29 -0400

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -34,6 +34,7 @@ public class SqlInjectionLesson8 extends AssignmentEndpoint {
     protected AttackResult injectableQueryConfidentiality(String name, String auth_tan) {
         StringBuffer output = new StringBuffer();
         String query = "SELECT * FROM employees WHERE last_name = '" + name + "' AND auth_tan = '" + auth_tan + "'";
+        String teststring = "";
 
         try (Connection connection = dataSource.getConnection()) {
             try {
